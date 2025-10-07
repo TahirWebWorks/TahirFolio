ea46ed5b52bac1da7b38be57e10b30edc02c9a14
TahirWebWorks <tahirwebworks@gmail.com>
2025-10-07 20:24:05 +0500

    Portfolio project to showcase the Works

--- src/pages/home/components/hero_section.jsx
+++ src/pages/home/components/hero_section.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
   return (
     <div className="flex justify-between items-center py-20">
       <div className="w-1/2">
-        <h2 className="text-3xl font-semibold ">I am Tahir</h2>
+        <h2 className="text-3xl font-semibold ">I am Tahir,</h2>
         <h2 className="text-7xl/20 font-semibold line my-3">Web Developer + Web Designer</h2>
         <p className="text-xl/8 font-light w-[470px]">I break down complex user experience problems to create integrity focussed solutions that connect billions of people.</p>
       </div>
