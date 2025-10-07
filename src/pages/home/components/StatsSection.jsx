b25d0e9c3ba3bafde150cf8b240de423d54d8fd6
TahirWebWorks <tahirwebworks@gmail.com>
2025-10-07 21:53:28 +0500

    Refactor HeroSection and StatsSection components: add new HeroSection with gradient text and download button, and implement StatsSection for displaying experience and project statistics.

---
+++ src/pages/home/components/StatsSection.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function StatsSection() {
+  return (
+    <div className="grid grid-cols-4 grid-rows-2 my-24 gap-8 ">
+      <div className="p-10 rounded-2xl col-span-2 row-span-2 bg-gradient-to-br from-10% via-[#211933] to-[#8C52FF]">
+        <div>
+        <div className="flex items-center gap-10">
+        <h2 className="text-[150px] font-bold text-gray-900">5+</h2>
+        <h3 className="text-4xl font-semibold">
+          Years Of Experience
+        </h3>
+        </div>
+        <p className="text-gray-600 text-lg leading-relaxed">
+          Crafting digital experiences through clean code and intuitive design.
+          I specialize in creating responsive, user-friendly websites and
+          applications that drive results and engage audiences.
+        </p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center text-center bg-[#191818] rounded-2xl">
+        <div className="text-3xl font-bold">150+</div>
+        <div className="text-gray-600 mt-2">Projects Completed</div>
+      </div>
+      <div className="flex flex-col items-center justify-center text-center bg-[#191818] rounded-2xl">
+        <div className="text-3xl font-bold">50+</div>
+        <div className="text-gray-600 mt-2">UI/UX Designs</div>
+      </div>
+      <div className="flex flex-col items-center justify-center text-center bg-[#191818] rounded-2xl">
+        <div className="text-3xl font-bold">98%</div>
+        <div className="text-gray-600 mt-2">Client Satisfaction</div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center text-center bg-[#191818] rounded-2xl">
+        <div className="text-3xl font-bold ">40+</div>
+        <div className="text-gray-600 mt-2">Happy Clients</div>
+      </div>
+    </div>
+  );
+}
+
+export default StatsSection;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import HeroSection  from './components/hero_section'
+import HeroSection  from './components/heroSection'
+import StatsSection from './components/StatsSection';
 
 function Home(){
     return(
         <div>
            <HeroSection/>
+           <StatsSection/>
         </div>
     )
 }
