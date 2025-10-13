dbeb70db20778892af795ae91604800d58d12133
TahirWebWorks <tahirwebworks@gmail.com>
2025-10-13 17:36:22 +0500

    Fix import casing for Header and HeroSection components in App and Home pages

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -1,5 +1,3 @@
-import {ArrowRight} from "lucide-react";
-
 export const servicesData = [
     {id: 1, title: "MERN Stack", short_description: "Conducting qualitative and quantitative research to understand user needs, behaivours, and pain points, Utilizing methods..."},
     {id: 2, title: "NEXT.JS", short_description: "Conducting qualitative and quantitative research to understand user needs, behaivours, and pain points, Utilizing methods..."},
